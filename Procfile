815216cd3d50320e79d5e8922b6adfc6c79eb10e
mk <twelvegold@gmail.com>
2022-05-20 13:34:01 +0900

    makeatotalbase

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fwhwork.wsgi --log-file -
--- fwhwork/wsgi.py
+++ fwhwork/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fwhwork project.
+WSGI config for atotalbase project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fwhwork.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
